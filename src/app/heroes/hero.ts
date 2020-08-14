96c0d5f003f84bd2d0a4e4f57ecacdbbf8723611
bbros <bbros@bbrosui-MacBookPro.local>
2020-08-14 16:13:07 +0900

    modified hero mock data and some styles

--- src/app/heroes/hero.ts
+++ src/app/heroes/hero.ts
@@ -2,11 +2,5 @@ export interface Hero {
   id: number;
   name?: string;
   thumbImgUrl?: string;
+  introText?: string;
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
