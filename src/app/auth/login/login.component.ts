2131179359c642357961d9f59ecaad7b67647a8c
bbros <bbros@bbrosui-MacBookPro.local>
2020-08-18 10:50:30 +0900

    converted async func

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,7 @@ export class LoginComponent implements OnInit {
   isLoggedIn = false;
   isPending = false;
 
-  constructor(public authService: AuthService, public router: Router) {}
+  constructor(public authService: AuthService, public router: Router) { }
 
   getMessgae() {
     return this.isPending
@@ -25,22 +26,49 @@ export class LoginComponent implements OnInit {
       .subscribe((isLoggedIn = false) => (this.isLoggedIn = isLoggedIn));
   }
 
-  handleSubmit(event: Event) {
+  //펜딩 체크 위해 지연 - es5
+  // handleSubmit(event: Event) {
+  //   event.preventDefault();
+  //   try {
+  //     if (!this.isLoggedIn) {
+  //       this.isPending = true;
+  //       // pending 상태 체크를 위해 이벤트 루프 지연
+  //       setTimeout(() => {
+  //         const redirect = this.authService.login();
+  //         this.router.navigate([redirect]);
+  //         this.isPending = false;
+  //       }, 1000);
+  //     } else {
+  //       this.authService.logout();
+  //     }
+  //   } catch {
+  //     this.router.navigate(['500']);
+  //   }
+  // }
+
+  //펜딩 체크 위해 지연 - es7
+
+  pendingStateCheck() {
+    const redirect = this.authService.login();
+    this.router.navigate([redirect]);
+    return new Promise((resolve, reject) => {
+      resolve(this.isPending = false)
+    });
+  }
+
+  async handleSubmit(event: Event) {
     event.preventDefault();
     try {
       if (!this.isLoggedIn) {
         this.isPending = true;
-        // pending 상태 체크를 위해 이벤트 루프 지연
-        setTimeout(() => {
-          const redirect = this.authService.login();
-          this.router.navigate([redirect]);
-          this.isPending = false;
-        }, 1000);
+        const resultOfPendingState = await this.pendingStateCheck();
+        console.log(resultOfPendingState, '@@');
+        // this.isPending = resultOfPendingState;
       } else {
         this.authService.logout();
       }
     } catch {
       this.router.navigate(['500']);
-    }
+    };
   }
 }
